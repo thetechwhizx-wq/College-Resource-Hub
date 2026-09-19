ab646cfa2010ececc01a8ee5207702f5de0e2a13
karthik <git config --global user.email your-copied-email@users.noreply.github.com>
2026-09-19 15:35:29 +0100

    Phase 2: connected announcements.html and resource.html to backend via fetch

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -34,3 +34,47 @@ if (assignmentsList) {
       assignmentsList.innerHTML = "<p>Could not load assignments. Is the backend running?</p>";
     });
 }
+const announcementsList = document.getElementById("announcementsList");
+
+if (announcementsList) {
+  fetch("http://localhost:3000/api/announcements")
+    .then(response => response.json())
+    .then(announcements => {
+      announcements.forEach(announcements => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+          <h3>${announcements.title}</h3>
+          <p>title: ${announcements.title} | date: ${announcements.date}</p>
+        `;
+        announcementsList.appendChild(card);
+      });
+    })
+    .catch(error => {
+      console.error("Failed to load assignments:", error);
+      announcementsList.innerHTML = "<p>Could not load assignments. Is the backend running?</p>";
+    });
+}
+const resourcesList = document.getElementById("resourcesList");
+
+if (resourcesList) {
+  fetch("http://localhost:3000/api/resources")
+    .then(response => response.json())
+    .then(resources => {
+      resources.forEach(resource => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+          <h3>${resource.title}</h3>
+          <p>Subject: ${resource.subject} | Unit: ${resource.unit}</p>
+          <a href="#" class="btn">View</a>
+          <a href="#" class="btn">Download</a>
+        `;
+        resourcesList.appendChild(card);
+      });
+    })
+    .catch(error => {
+      console.error("Failed to load resources:", error);
+      resourcesList.innerHTML = "<p>Could not load resources. Is the backend running?</p>";
+    });
+}
